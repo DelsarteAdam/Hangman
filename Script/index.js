edf71bbdc4247170e96c7dd0780cc67a66cd1139
Adam <delsarte.adam@gmail.com>
2024-06-17 00:01:35 +0200

    update02

--- Script/index.js
+++ Script/index.js
@@ -1,11 +1,26 @@
 import { words } from "./wordsList.js";
 
+let hangman = [
+  "potence.png",
+  "potencehead.png",
+  "potencearm1.png",
+  "ipotencearm2.png",
+  "potencebody.png",
+  "potenceleg1.png",
+  "potenceleg2.png",
+];
+//let arrWord = [];
+let gameOn = false;
+
 let win = false;
 let lose = false;
-
-//lose point + fail check
-
 let losePoint = 0;
+document.getElementById("game").hidden = true;
+document.getElementById("inputletter").hidden = true;
+document.getElementById("newGSection").hidden = false;
+let arrWord = [];
+let letterSubmit = "";
+//lose point + fail check
 
 function loseCondition(losePoint) {
   if (losePoint == 6) {
@@ -13,42 +28,76 @@ function loseCondition(losePoint) {
   }
 }
 
-//select random word from list
-console.log(words.length);
-let numberWordSelect = Math.floor(Math.random() * 2999);
-console.log(numberWordSelect);
-let worldSelect = words[numberWordSelect];
-console.log(worldSelect);
-let arrWord = worldSelect.split("");
-console.log(arrWord);
-//show not show
-document.getElementById("game").hidden = false;
-
-//add html space for the word
-//create all boxes
-let letterSubmit = "a";
-let a = "";
-for (let i = 0; i < arrWord.length; i++) {
-  if (letterSubmit == arrWord[i]) {
-    a += '<p class="lettershow">' + arrWord[i] + "</p>";
-  }
-  a += '<p class="lettershow"></p>';
-}
-document.getElementById("guessword").innerHTML = a;
-
-//get chose letter
-// check letter and print
+// check letter
 
 function letterCheck() {
   let l = 0;
   for (let i = 0; i < arrWord.length; i++) {
-    if (letterSubmit != arrWord[i]) {
-      l = 1;
+    if (letterSubmit == arrWord[i]) {
+      l = 0;
+      break;
     }
-    l = 0;
+    l = 1;
   }
   losePoint += l;
 }
-letterCheck();
 
-console.log(losePoint);
+//add html space for the word
+
+// change letter base html
+
+function changeBaseHtml() {
+  for (let i = 0; i < arrWord.length; i++) {
+    if (letterSubmit == arrWord[i]) {
+      let b = "letterPose" + i;
+      console.log(b);
+      document.getElementById(b).innerHTML = letterSubmit;
+    }
+  }
+}
+
+//hangman image change
+
+function imageChange() {
+  let imageLose = hangman[losePoint];
+  document.getElementById("hangman").src = "images/" + imageLose;
+}
+
+// new game
+document.getElementById("newGame").addEventListener("click", function () {
+  //show not show
+  document.getElementById("game").hidden = false;
+  document.getElementById("inputletter").hidden = false;
+  document.getElementById("newGSection").hidden = true;
+
+  win = false;
+  lose = false;
+  losePoint = 0;
+
+  //select random word from list
+
+  let numberWordSelect = Math.floor(Math.random() * 2999);
+  let worldSelect = words[numberWordSelect];
+  arrWord = worldSelect.split("");
+  console.log(worldSelect);
+  console.log(arrWord);
+
+  //create all boxes
+
+  function wordBaseHtml() {
+    let a = "";
+    for (let i = 0; i < arrWord.length; i++) {
+      a += '<p class="lettershow" id="letterPose' + i + '" ></p>';
+    }
+    document.getElementById("guessword").innerHTML = a;
+  }
+
+  wordBaseHtml();
+});
+
+//input game
+
+document.getElementById("run").addEventListener("click", function () {
+  letterSubmit = document.getElementById("text1");
+  console.log(letterSubmit);
+});
